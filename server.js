b0ef233501c5db963c70686c155eed8c379733f4
Shash938 <shashwat.dwivedi@bcah.christuniversity.in>
2025-03-24 00:14:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,15 +3,12 @@ const path = require('path');
 const app = express();
 const PORT = 8080;
 
-// Serve static files (HTML, CSS, JS)
 app.use(express.static(path.join(__dirname)));
 
-// Serve the wars.json file
 app.get('/wars.json', (req, res) => {
     res.sendFile(path.join(__dirname, 'wars.json'));
 });
 
-// Start the server
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
